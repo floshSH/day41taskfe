971b64180999fd7bd327673256359d148c5cd703
floshSH <ssubash042@gmail.com>
2024-08-19 21:14:44 +0530

    day41taskfe

---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='d-flex justify-content-center align-items-center' style={{width:"100vw"}}>
+        <h1>HOME PAGE</h1>
+    </div>
+  )
+}
+
+export default Home
